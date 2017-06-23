eba7d7ab5ba68ed2b56bf70aaef16bddc7fbb4fb
fsilva <fsilva@stone.com.br>
2017-06-23 17:53:47 -0300

    new launch icon and buttons start logic

--- app/src/main/java/com/fernandobarbosa/tictactoe/MainActivity.kt
+++ app/src/main/java/com/fernandobarbosa/tictactoe/MainActivity.kt
@@ -2,11 +2,33 @@ package com.fernandobarbosa.tictactoe
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    protected fun btnClick(view : View) {
+        val btnSelected = view as Button
+        var cellId = 0
+
+        when (btnSelected.id) {
+            R.id.btn1 -> cellId = 1
+            R.id.btn2 -> cellId = 2
+            R.id.btn3 -> cellId = 3
+            R.id.btn4 -> cellId = 4
+            R.id.btn5 -> cellId = 5
+            R.id.btn6 -> cellId = 6
+            R.id.btn7 -> cellId = 7
+            R.id.btn8 -> cellId = 8
+            R.id.btn9 -> cellId = 9
+        }
+
+        Toast.makeText(this,  "ID: " + cellId, Toast.LENGTH_SHORT).show()
     }
 }
