f81494c97360f2a08c09267fbe2357bc1aded1a3
fsilva <fsilva@stone.com.br>
2017-06-23 18:27:22 -0300

    refact checkWinner method

--- app/src/main/java/com/fernandobarbosa/tictactoe/MainActivity.kt
+++ app/src/main/java/com/fernandobarbosa/tictactoe/MainActivity.kt
@@ -58,43 +58,19 @@ class MainActivity : AppCompatActivity() {
     fun checkWinner() {
         var winner = -1
 
-        if (player1.contains(1) && player1.contains(2) && player1.contains(3)) {
-            winner = 1;
-        }
-        if (player1.contains(4) && player1.contains(5) && player1.contains(6)) {
-            winner = 1;
-        }
-        if (player1.contains(7) && player1.contains(8) && player1.contains(9)) {
-            winner = 1;
-        }
-        if (player1.contains(1) && player1.contains(4) && player1.contains(7)) {
-            winner = 1;
-        }
-        if (player1.contains(2) && player1.contains(5) && player1.contains(8)) {
-            winner = 1;
-        }
-        if (player1.contains(3) && player1.contains(6) && player1.contains(6)) {
-            winner = 1;
-        }
+        if (player1.contains(1) && player1.contains(2) && player1.contains(3)) winner = 1
+        if (player1.contains(4) && player1.contains(5) && player1.contains(6)) winner = 1
+        if (player1.contains(7) && player1.contains(8) && player1.contains(9)) winner = 1
+        if (player1.contains(1) && player1.contains(4) && player1.contains(7)) winner = 1
+        if (player1.contains(2) && player1.contains(5) && player1.contains(8)) winner = 1
+        if (player1.contains(3) && player1.contains(6) && player1.contains(6)) winner = 1
 
-        if (player2.contains(1) && player2.contains(2) && player2.contains(3)) {
-            winner = 2;
-        }
-        if (player2.contains(4) && player2.contains(5) && player2.contains(6)) {
-            winner = 2;
-        }
-        if (player2.contains(7) && player2.contains(8) && player2.contains(9)) {
-            winner = 2;
-        }
-        if (player2.contains(1) && player2.contains(4) && player2.contains(7)) {
-            winner = 2;
-        }
-        if (player2.contains(2) && player2.contains(5) && player2.contains(8)) {
-            winner = 2;
-        }
-        if (player2.contains(3) && player2.contains(6) && player2.contains(6)) {
-            winner = 2;
-        }
+        if (player2.contains(1) && player2.contains(2) && player2.contains(3)) winner = 2
+        if (player2.contains(4) && player2.contains(5) && player2.contains(6)) winner = 2
+        if (player2.contains(7) && player2.contains(8) && player2.contains(9)) winner = 2
+        if (player2.contains(1) && player2.contains(4) && player2.contains(7)) winner = 2
+        if (player2.contains(2) && player2.contains(5) && player2.contains(8)) winner = 2
+        if (player2.contains(3) && player2.contains(6) && player2.contains(6)) winner = 2
 
         if (winner != -1) {
             if (winner == 1) {
